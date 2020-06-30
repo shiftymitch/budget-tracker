063e0b24bc90da901e82969ff8ba1b313230ac08
Mitch Henderson <shiftymitch@gmail.com>
2020-06-30 08:17:54 -0600

    fixing service-worker error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,15 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
   '/manifest.webmanifest',
   '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/icons/icon-152x152.png',
-  '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-384x384.png',
-  '/assets/images/icons/icon-512x512.png',
-  '/assets/images/1.jpg',
-  '/assets/images/2.jpg',
-  '/assets/images/3.jpg',
-  '/assets/images/4.jpg',
-  '/assets/images/5.jpg',
-  '/assets/images/6.jpg',
-  '/assets/images/7.jpg',
-  '/assets/images/8.jpg',
-  '/assets/images/9.jpg',
-  '/assets/images/10.jpg',
-  '/assets/images/11.jpg',
-  '/assets/images/12.jpg',
-  '/assets/images/13.jpg',
-  '/assets/images/14.jpg',
-  '/assets/images/15.jpg',
-  '/assets/images/16.jpg',
-  '/assets/images/17.jpg',
-  '/assets/images/18.jpg',
-  '/assets/images/19.jpg',
-  '/assets/images/20.jpg',
-  '/assets/images/21.jpg',
-  '/assets/images/22.jpg',
-  '/assets/images/23.jpg',
-  '/assets/images/24.jpg',
-  '/assets/images/25.jpg',
-  '/assets/images/26.jpg',
-  '/assets/images/27.jpg',
-  '/assets/images/28.jpg',
-  '/assets/images/29.jpg',
-  '/assets/images/30.jpg',
-  '/assets/images/31.jpg',
-  '/assets/images/32.jpg',
-  '/assets/images/33.jpg',
-  '/assets/images/34.jpg',
-  '/assets/images/35.jpg',
-  '/assets/images/36.jpg',
-  '/assets/images/37.jpg',
-  '/assets/images/38.jpg'
+  '/assets/js/db.js',
+  '/assets/js/index.js',
+  '/assets/img/icons/icon-192x192.png',
+  '/assets/img/icons/icon-512x512.png',
+  '/assets/img/6.png',
+  '/assets/img/cx_logo.svg',
+  '/assets/img/shiftycx_logo_white.png'
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -86,7 +45,7 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {
+  if (evt.request.url.includes("/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
